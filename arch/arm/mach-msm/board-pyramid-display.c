aca587111b2c1813f5bd163930d63863ed8d46f4
Ivan Grinko <iivanich@gmail.com>
2014-01-15 19:50:25 +0200

    board-pyramid-display.c: Increase clocks

--- arch/arm/mach-msm/board-pyramid-display.c
+++ arch/arm/mach-msm/board-pyramid-display.c
@@ -833,7 +833,7 @@ int pyramid_mdp_gamma(void)
 
 static struct msm_panel_common_pdata mdp_pdata = {
 	.gpio = GPIO_LCD_TE,
-	.mdp_max_clk = 200000000,
+	.mdp_max_clk = 266667000,
 	.mdp_max_bw = 2000000000,
 	.mdp_bw_ab_factor = 115,
 	.mdp_bw_ib_factor = 150,
